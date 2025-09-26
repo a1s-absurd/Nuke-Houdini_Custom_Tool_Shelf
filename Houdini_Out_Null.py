35e041dd06db5179c5ac4236a3552700e34bf186
a1s-absurd <swamyanshul1992@gmail.com>
2025-09-26 23:25:27 +0530

    Update Houdini_Out_Null.py

--- Houdini_Out_Null.py
+++ Houdini_Out_Null.py
@@ -10,22 +10,20 @@ def add_out_nulls_to_selected():
     for node in selected_nodes:
         parent = node.parent()
 
-        # Build name: _OUT_<originalname>
+       
         out_name = "_OUT_" + node.name()
 
-        # Create Null node
+      
         null_node = parent.createNode("null", out_name)
 
-        # Connect it to the selected node
         null_node.setInput(0, node)
 
-        # Move to good position
         null_node.moveToGoodPosition()
 
-        # Color black (RGB 0,0,0)
-        null_node.setColor(hou.Color((0, 0, 0)))
+      
+        null_node.setColor(hou.Color((0, 1, 0)))
 
-        # Optionally select the new null node
+        
         null_node.setSelected(True, clear_all_selected=False)
 
     hou.ui.displayMessage("Added _OUT_ Null nodes to {} node(s).".format(len(selected_nodes)))
